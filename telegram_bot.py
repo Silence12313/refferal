c9bc2b830309b8db21305686ced35ace01980481
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-12 16:38:56 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,10 +4,17 @@ from aiogram.filters import Command
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-from config import BOT_TOKEN, CHANNEL_ID, CHANNEL_LINK, BOT_USERNAME, REF_REQUIRED
+from config import BOT_TOKEN, CHANNEL_ID, CHANNEL_LINK, REF_REQUIRED
 from keyboards import main_keyboard
-from database import add_user, confirm_referral, referral_owner, count_referrals, referral_link, export_users
-
+from database import (
+    add_user,
+    add_referral,
+    confirm_referral,
+    referral_owner,
+    count_referrals,
+    referral_link,
+    export_users
+)
 
 bot = Bot(
     token=BOT_TOKEN,
@@ -20,12 +27,10 @@ dp = Dispatcher()
 async def check_subscription(user_id):
 
     try:
+
         member = await bot.get_chat_member(CHANNEL_ID, user_id)
 
-        if member.status in ["member", "administrator", "creator"]:
-            return True
-
-        return False
+        return member.status in ["member", "administrator", "creator"]
 
     except:
         return False
@@ -43,17 +48,19 @@ async def start(message: Message):
 
     add_user(
         message.from_user.id,
-        message.from_user.first_name,
         message.from_user.username,
-        ref
+        message.from_user.first_name
     )
+
+    if ref:
+        add_referral(int(ref), message.from_user.id)
 
     text = f"""
 Рады видеть вас, {message.from_user.first_name}!
 
 Если вам нравится наш канал [Your art muse]({CHANNEL_LINK}), не стесняйтесь рекомендовать его друзьям!
 
-*Пригласите 3-х своих друзей и получите в подарок наш супер-бонус!*
+*Пригласите {REF_REQUIRED} друзей и получите бонус!*
 
 База из 5 эфиров с разборами искусства 🔥
 """
@@ -70,7 +77,10 @@ async def get_link(callback: CallbackQuery):
 
     if not await check_subscription(user_id):
 
-        await callback.message.answer("Сначала подпишитесь на канал.")
+        await callback.message.answer(
+            "Сначала подпишитесь на канал."
+        )
+
         return
 
     confirm_referral(user_id)
@@ -81,10 +91,15 @@ async def get_link(callback: CallbackQuery):
 
         count = count_referrals(owner)
 
-        await bot.send_message(
-            owner,
-            f"🎉 Новый реферал!\nВсего: {count}/{REF_REQUIRED}"
-        )
+        try:
+
+            await bot.send_message(
+                owner,
+                f"🎉 Новый реферал!\nВсего: {count}/{REF_REQUIRED}"
+            )
+
+        except:
+            pass
 
         if count >= REF_REQUIRED:
 
@@ -93,13 +108,9 @@ async def get_link(callback: CallbackQuery):
                 """
 Поздравляем!
 
-Вы победили в нашем конкурсе и получаете наш супер-бонус!
+Вы победили в конкурсе и получаете бонус!
 
-База из 5 эфиров:
 https://www.youtube.com/playlist?list=PL2DjtAFoLP6w3ztMXg4eLzBPUj3iaFvPm
-
-Бесплатная консультация:
-https://onstudy.org/konsultatsiya-art/
 """
             )
 
@@ -116,13 +127,13 @@ async def how(callback: CallbackQuery):
     await callback.answer()
 
     text = f"""
-Что вам нужно сделать, чтобы получить эфиры:
+Что нужно сделать:
 
-- подпишитесь на канал [Your art muse]({CHANNEL_LINK})
-- нажмите кнопку «получить ссылку»
-- отправьте ссылку друзьям
-- пригласите {REF_REQUIRED} друзей
-- получите доступ к эфиру
+1. Подпишитесь на канал
+2. Нажмите «получить ссылку»
+3. Отправьте её друзьям
+4. Пригласите {REF_REQUIRED} друзей
+5. Получите бонус
 """
 
     await callback.message.answer(text)
