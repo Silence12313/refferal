14c571d3f3b31689b9854ef58472d738a78f06d5
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-12 16:38:28 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
 import sqlite3
 from datetime import datetime
+import io
 
-conn = sqlite3.connect("bot.db", check_same_thread=False)
+from config import BOT_USERNAME
+
+conn = sqlite3.connect(
+    "bot.db",
+    check_same_thread=False,
+    timeout=30
+)
+
 cursor = conn.cursor()
 
 cursor.execute("PRAGMA journal_mode=WAL")
-
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
@@ -15,7 +22,6 @@ first_name TEXT,
 joined_at TEXT
 )
 """)
-
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS referrals(
@@ -79,6 +85,21 @@ def confirm_referral(invited_id):
     conn.commit()
 
 
+def referral_owner(invited_id):
+
+    cursor.execute(
+        "SELECT referrer_id FROM referrals WHERE invited_id=?",
+        (invited_id,)
+    )
+
+    row = cursor.fetchone()
+
+    if row:
+        return row[0]
+
+    return None
+
+
 def count_referrals(referrer_id):
 
     cursor.execute(
@@ -89,6 +110,11 @@ def count_referrals(referrer_id):
     return cursor.fetchone()[0]
 
 
+def referral_link(user_id):
+
+    return f"https://t.me/{BOT_USERNAME}?start={user_id}"
+
+
 def get_all_users():
 
     cursor.execute("SELECT * FROM users")
@@ -96,18 +122,16 @@ def get_all_users():
     return cursor.fetchall()
 
 
-def get_referral_report():
+def export_users():
 
-    cursor.execute("""
-    SELECT
-    r.referrer_id,
-    u1.username,
-    r.invited_id,
-    u2.username,
-    r.confirmed
-    FROM referrals r
-    LEFT JOIN users u1 ON u1.id = r.referrer_id
-    LEFT JOIN users u2 ON u2.id = r.invited_id
-    """)
+    users = get_all_users()
 
-    return cursor.fetchall()
+    text = ""
+
+    for u in users:
+        text += f"{u[0]} | {u[1]} | {u[2]} | {u[3]}\n"
+
+    file = io.BytesIO(text.encode())
+    file.name = "users.txt"
+
+    return file
