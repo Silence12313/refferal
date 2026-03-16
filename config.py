3603d2ddd074b582aa92694506f395cb32a5f325
Silence <q2fftfj795@privaterelay.appleid.com>
2026-03-16 22:54:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 BOT_TOKEN = "8233660891:AAHuLsyErkasMdsYXuLJFD_D3ax2TsR6OFI"
 
+BOT_TOKEN = "8233660891:AAHuLsyErkasMdsYXuLJFD_D3ax2TsR6OFI"
+
 BOT_USERNAME = "Your_art_muse_new_bot"
 
 CHANNEL_ID = -1001687144236
@@ -8,6 +10,4 @@ CHANNEL_LINK = "https://t.me/your_art_muse"
 ADMIN_IDS = [5289442566]
 
 REF_REQUIRED = 3
-
-
-MAX_BOT_TOKEN = "PUT_MAX_BOT_TOKEN"
+APP_URL = "https://refferal-production.up.railway.app"
